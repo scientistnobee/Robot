d3ba771be65fe94bc5cd79c4411a10c3b627fe17
scientistnobee <scientistnobee@gmail.com>
2014-06-24 14:37:43 +0100

    Update to_run2.sh

--- to_run2.sh
+++ to_run2.sh
@@ -6,6 +6,7 @@ do
  
   RESULT=$(python find_img.py)
   echo "$RESULT"
+ #backup as png file, later delete all jpg files
   cp $RESULT $RESULT.png
   sudo ./deepbelief $RESULT > try4.txt
   RESULT2=$(python find_max.py)
