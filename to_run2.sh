7fecb54e625a9b89f6f3cc836f05fd3dbca93ca9
scientistnobee <scientistnobee@gmail.com>
2014-06-24 15:11:59 +0100

    Update to_run2.sh

--- to_run2.sh
+++ to_run2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#sudo service motion start
-#sleep 30
+sudo service motion start
+sleep 30
 while [ 1 ]
 do
- 
+
   RESULT=$(python find_img.py)
   echo "$RESULT"
  #backup as png file, later delete all jpg files
@@ -11,8 +11,8 @@ do
   sudo ./deepbelief $RESULT > try4.txt
   RESULT2=$(python find_max.py)
   echo "$RESULT2"
-sleep 1
+sleep 60
 done
-
+wait
 #trap sudo rm *jpg EXIT
 
